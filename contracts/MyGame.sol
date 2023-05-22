ea197123d7fb28feacd2d5dc54be4da9243390f6
Paul Tsviliuk <paultsvilyuk@gmail.com>
2023-05-22 16:49:41 -0700

    Added tokenURI function to attach nftHolderAttributes by overriding tokenURI. Added base64 helper to convert data easily.

--- contracts/MyGame.sol
+++ contracts/MyGame.sol
@@ -8,6 +8,8 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "hardhat/console.sol";
 
+import "./libraries/Base64.sol";
+
 contract MyGame is ERC721 {
   // We'll hold our character's attributes in a struct. 
   struct CharacterAttributes {
@@ -91,5 +93,32 @@ contract MyGame is ERC721 {
 
     // Increment the tokenId for the next person that uses it.
     _tokenIds.increment();
+  }
+
+  function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+    CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+
+    string memory strHp = Strings.toString(charAttributes.hp);
+    string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+    string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
+
+    string memory json = Base64.encode(
+      abi.encodePacked(
+        '{"name": "',
+        charAttributes.name,
+        ' -- NFT #: ',
+        Strings.toString(_tokenId),
+        '", "description": "This is an NFT that lets people play in the game Rupture of the bast shoe in the air!", "image": "',
+        charAttributes.imageURI,
+        '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',
+        strAttackDamage,'} ]}'
+      )
+    );
+
+    string memory output = string(
+      abi.encodePacked("data:application/json;base64,", json)
+    );
+    
+    return output;
   }
 }
